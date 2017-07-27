f78cfc170486795e166e9d66634591ecae99d398
Yuxuan Zhao <yzhao9@ualberta.ca>
2017-07-26 20:39:04 -0600

    design home page

--- trakr/urls.py
+++ trakr/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     url(r'^$', views.index, name="index"), 
     url(r'^home$', views.home, name="home"),
     url(r'^status$', views.status, name="status"),
-    # url(r'^account$', views.account, name="account"),
+    url(r'^account$', views.account, name="account"),
     url(r'^loadUserData$', views.loadUserData, name="loadUserData")
 
     
